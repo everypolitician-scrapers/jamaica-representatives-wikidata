6f701976f726745bb9303c276a3b1c7991473fbc
Tony Bowden <tony@tmtm.com>
2017-04-05 07:20:15 +0100

    Always look up previously-matched people
    This isn't a great approach, as it means we don't notice upstream
    changes that we probably should, but we don't have a good enough source
    for Jamaican data to handle this in a much better way yet.

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
+existing = EveryPolitician::Index.new.country("Jamaica").lower_house.popolo.persons.map(&:wikidata).compact
+
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/jamaica-representatives-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: names })
